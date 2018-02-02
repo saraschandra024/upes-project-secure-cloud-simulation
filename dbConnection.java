a88f21e7ea7a4144cc77362822237d52c3090741
saraschandra024 <saraschandra024@gmail.com>
2018-02-02 01:42:53 -0800

    Create dbConnection.java

---
+++ dbConnection.java
@@ -0,0 +1,22 @@
+
+package connection;
+
+import java.sql.PreparedStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class dbConnection {
+
+    static Connection con = null;
+    PreparedStatement pst = null;
+
+    public static Connection makeConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/cryptography", "abhi", "root");
+
+        } catch (Exception e) {
+        }
+        return con;
+    }
+}
